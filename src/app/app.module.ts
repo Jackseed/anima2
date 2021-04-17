f6b2048dc1b764bb5a3335522a3bfeb668bb56b3
Jackseed <vinkho.c@gmail.com>
2021-04-17 13:38:36 +0200

    install akita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
+  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, environment.production ? [] : AkitaNgDevtools.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
