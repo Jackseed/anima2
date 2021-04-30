1c63c6d7d4590be717329f6b9a2a3d7713d5e6f3
Jackseed <vinkho.c@gmail.com>
2021-04-30 08:44:14 +0200

    era & playerIds

--- src/app/games/_state/game.model.ts
+++ src/app/games/_state/game.model.ts
@@ -1,13 +1,26 @@
 export interface Game {
   id?: string;
   name?: string;
+  playerIds?: string[];
+  era?: {
+    remainingActions: number;
+    eraCounters: number;
+    turnCount: number;
+  };
 }
 
+export const actionPerTurn = 2;
 
 export function createGame(params: Partial<Game> = {}): Game {
   return {
     id: params.id,
     name: params.name,
+    playerIds: params.playerIds,
+    era: {
+      remainingActions: actionPerTurn,
+      eraCounters: 1,
+      turnCount: 1,
+    },
     ...params,
   };
 }
--- src/app/games/_state/game.service.ts
+++ src/app/games/_state/game.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { CollectionConfig, CollectionService } from 'akita-ng-fire';
+import { UserQuery } from 'src/app/auth/_state';
 import { createGame } from './game.model';
 import { GameStore, GameState } from './game.store';
 
 @Injectable({ providedIn: 'root' })
 @CollectionConfig({ path: 'games' })
 export class GameService extends CollectionService<GameState> {
-  constructor(store: GameStore) {
+  constructor(store: GameStore, private afAuth: AngularFireAuth) {
     super(store);
   }
 
   public async createNewGame(name: string): Promise<string> {
     const id = this.db.createId();
-    const game = createGame({ id, name });
+    const playerIds = [(await this.afAuth.currentUser).uid];
+    const game = createGame({ id, name, playerIds });
+
     // Create the game
     await this.collection
       .doc(id)
