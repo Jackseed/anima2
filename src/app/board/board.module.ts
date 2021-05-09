a3c45b47fe4a255064906b843035b41807b23556
Jackseed <vinkho.c@gmail.com>
2021-05-09 19:47:55 +0200

    footer & buttons"

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Components
 import { BoardViewComponent } from './board-view/board-view.component';
+import { FooterComponent } from './footer/footer.component';
 // Flex Layout
 import { FlexLayoutModule } from '@angular/flex-layout';
 // Material
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [BoardViewComponent],
-  imports: [CommonModule, FlexLayoutModule, MatSnackBarModule, MatCardModule],
+  declarations: [BoardViewComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class BoardModule {}
