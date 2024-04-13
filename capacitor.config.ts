c429837dbc4e2c4208dd37ccea6832bcc52c5f1f
Jackseed <vinkho.c@gmail.com>
2024-04-13 12:26:48 +0200

    capacitor installed

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.anima2',
+  appName: 'anima2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
