cd461b462db17a02476cc56f4c90d8b7b5d032dd
Jackseed <vinkho.c@gmail.com>
2024-04-16 18:10:44 +0200

    1st build and changed name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.anima',
+  appId: 'com.anima2',
   appName: 'anima',
   webDir: 'public',
   server: {
