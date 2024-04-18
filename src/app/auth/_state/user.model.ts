4d9cff6587be50224d77c6872d9554a056660726
Jackseed <vinkho.c@gmail.com>
2024-04-18 16:24:10 +0200

    pseudo form

--- src/app/auth/_state/user.model.ts
+++ src/app/auth/_state/user.model.ts
@@ -9,6 +9,7 @@ export interface User {
 export function createUser(params: Partial<User>) {
   return {
     id: params.id,
+    username: params.name || '',
     gamePlayed: [],
     matchPlayed: 0,
     matchWon: 0,
