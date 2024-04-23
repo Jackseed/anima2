7dada4cbda04e856b924a05ca5242b97451a6f25
Jackseed <vinkho.c@gmail.com>
2024-04-23 16:33:13 +0200

    1.4

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.anima2',
-  appName: 'anima',
+  appName: 'Anima',
   webDir: 'public',
   server: {
     androidScheme: 'https'
