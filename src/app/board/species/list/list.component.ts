66a7759ce19643f37a582e279e0f92c6a39e42a9
Jackseed <vinkho.c@gmail.com>
2024-04-25 10:23:06 +0200

    species list sorting

--- src/app/board/species/list/list.component.ts
+++ src/app/board/species/list/list.component.ts
@@ -14,6 +14,7 @@ import {
 } from '../../../board/species/_state/species.model';
 import { AbilityService } from '../../ability.service';
 import { TileQuery } from '../../tiles/_state';
+import { PlayerQuery } from '../../players/_state';
 
 export interface active {
   species: TileSpecies;
@@ -37,10 +38,28 @@ export class ListComponent implements OnInit {
     public dialogRef: MatDialogRef<ListComponent>,
     private snackbar: MatSnackBar,
     private tileQuery: TileQuery,
+    private playerQuery: PlayerQuery,
     private abilityService: AbilityService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const activePlayerId = this.playerQuery.getActiveId();
+    // Sort per player
+    this.data.speciesToList.sort((a, b) => {
+      if (a.playerId === activePlayerId) return -1;
+      if (b.playerId === activePlayerId) return 1;
+      if (a.playerId === 'neutral') return 1;
+      if (b.playerId === 'neutral') return -1;
+      return 0;
+    });
+    this.data.speciesToList.sort((a, b) => {
+      // If the playerIds are the same, sort by the length of tileIds
+      if (a.playerId === b.playerId) {
+        return this.getSpeciesCount(b) - this.getSpeciesCount(a);
+      }
+      return 0;
+    });
+  }
 
   // Either returns the global species quantity or the tile species quantity
   public getSpeciesCount(species: Species) {
